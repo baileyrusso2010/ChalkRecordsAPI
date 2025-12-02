ea18b762c120b01b745711fc569ecea41b559b81
Bailey Russo <baileyrusso3371@gmail.com>
2025-12-02 13:44:20 -0500

    Removed manual intervention for database

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,29 @@
 // src/database.ts
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize"
+
+const node_env = String(process.env.NODE_ENV)
+let dialectOptions
+
+if (node_env != "development") {
+    dialectOptions = {
+        ssl: {
+            required: true,
+            rejectUnauthorized: false,
+        },
+    }
+} else {
+    dialectOptions = {}
+}
 
 const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: String(process.env.DB_HOST), // or 'localhost'
-  port: Number(process.env.DB_PORT),
-  database: String(process.env.DB_DATABASE),
-  username: String(process.env.DB_USERNAME),
-  password: String(process.env.DB_PASSWORD),
-  logging: false, // optional
-  dialectOptions: {
-    ssl: {
-      required: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+    dialect: "postgres",
+    host: String(process.env.DB_HOST), // or 'localhost'
+    port: Number(process.env.DB_PORT),
+    database: String(process.env.DB_DATABASE),
+    username: String(process.env.DB_USERNAME),
+    password: String(process.env.DB_PASSWORD),
+    logging: false, // optional
+    dialectOptions: dialectOptions,
+})
 
-export default sequelize;
+export default sequelize
